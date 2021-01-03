e41487d8ee98bfe5800b24b43cac22a709820244
Victoria Thorsen <victoriathorsenn@gmail.com>
2021-01-03 16:16:54 -0500

    commit tree f6b096236627eafe9be493167093318144eb7d5a

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
 
+    @@all =[]
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def deposit(deposit_amount)
+        self.balance += deposit_amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        balance > 0 && status == "open"
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
